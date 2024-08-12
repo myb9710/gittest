ba179ed709bdb54fc679c2640483ed6df13ec5fe
myb <myb@tongji.edu.cm>
2024-08-12 02:35:10 -0700

    add a cpp file

---
+++ maketestfile.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+     int m,n,i,j;
+     float **Vp;
+     FILE *fid;
+//============================================
+     m=10;n=10;
+     Vp=new float*[m];
+     for(i=0;i<m;i++)
+     {
+         Vp[i]=new float[n];
+	 for(j=0;j<n;j++)
+	     Vp[i][j]=10*i+j+1001;
+     }
+//=============================================
+    ofstream out1("test2.dat");
+    for(i=0;i<m;i++)
+    for(j=0;j<n;j++)
+    out1.write((char*) &Vp[i][j],sizeof(float));
+    out1.close();
+
+     return 0;
+}
